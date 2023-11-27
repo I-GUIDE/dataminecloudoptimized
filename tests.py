1ff18b477d26fb0e1094adf06f4bf9325737b2f9
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2023-11-27 03:24:52 -0500

    Added tests.py

---
+++ tests.py
@@ -0,0 +1,32 @@
+import unittest
+
+# AORC Forcing Data description - https://rb.gy/8gnkls
+# TODO: need to look at (mean) temperature, humidity, precipitation etc.
+# TODO: need to stretch in the temporal direction with varying time values
+# TODO: maybe write a script to write out TCs
+
+
+class TestAdd(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual();
+
+    def test_2(self):
+        self.assertEqual();
+
+    def test_3(self):
+        self.assertEqual();
+
+    def test_4(self):
+        self.assertEqual();
+
+    def test_5(self):
+        self.assertEqual();
+
+    def test_6(self):
+        self.assertEqual();
+
+    def test_7(self):
+        self.assertEqual();
+
+if __name__ == '__main__':
+    unittest.main()
